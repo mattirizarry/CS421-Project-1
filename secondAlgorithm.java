4263a06189e077f889457f57e525721acc4325d7
Matthew Irizarry <42564599+mattirizarry@users.noreply.github.com>
2023-09-29 23:41:55 -0500

    Update secondAlgorithm.java

--- secondAlgorithm.java
+++ secondAlgorithm.java
@@ -1,8 +1,14 @@
 public class SecondAlgorithm {
     public static void main(String[] args) {
-        int[] arr = { 99,33,44,23,51,632,4,1 };
+        int[] case1 = { 99,33,44,23,51,632,4,1 };
+        int[] case2 = { 51,4156,456,489,589,48924,28};
+        int[] case3 = { 4564,456,456,4898489,21,87998,1234899,2145};
 
-        System.out.println(findMaximum(arr));
+        // Justification / Proving the algorithm works
+
+        System.out.println(findMaximum(case1));
+        System.out.println(findMaximum(case2));
+        System.out.println(findMaximum(case3));
     }
 
     public static int findMaximum(int[] arr, int left, int right) {
