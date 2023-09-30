005fca664943f11ff6456f9d557cb5264c58a813
Matthew Irizarry <42564599+mattirizarry@users.noreply.github.com>
2023-09-29 22:24:28 -0500

    Update firstAlgorithm.java

--- firstAlgorithm.java
+++ firstAlgorithm.java
@@ -1,17 +1,23 @@
 public class FirstAlgorithm {
 
     public static void main(String[] args) {
-        int[] arr = { 99,33,44,23,51,632,4,1 };
+        int[] case1 = { 99,33,44,23,51,632,4,1 };
+        int[] case2 = { 51,4156,456,489,589,48924,28};
+        int[] case3 = { 4564,456,456,4898489,21,87998,1234899,2145};
 
-        System.out.println(findMaximum(arr));
+        // Justification / Proving the algorithm works
+
+        System.out.println(findMaximum(case1));
+        System.out.println(findMaximum(case2));
+        System.out.println(findMaximum(case3));
     }
 
     public static int findMaximum(int[] arr) {
-        int max = arr[0]; // Assume the first element is the maximum
+        int max = arr[0];
 
         for (int num = 1; num < arr.length; num++) {
             if (arr[num] > max) {
-                max = arr[num]; // Update max if a larger number is found
+                max = arr[num];
             }
         }
 
